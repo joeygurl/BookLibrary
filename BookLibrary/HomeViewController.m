c3809f8d89f32d6d328439dba5faaba56cea3432
Diorelle Ramos <dramos30@gmail.com>
2014-04-12 16:13:06 -0500

    #1 Added Main Menu

--- BookLibrary/HomeViewController.m
+++ BookLibrary/HomeViewController.m
@@ -9,7 +9,7 @@
 #import "HomeViewController.h"
 
 @interface HomeViewController (){
-    NSArray *_menuArray;
+
 }
 
 @end
@@ -30,8 +30,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    _menuArray = [NSArray arrayWithObjects:@"My Library",@"Search Library", @"Settings", nil];
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -40,32 +39,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-#pragma mark - Table View
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-    return 1;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    return [_menuArray count];
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
-    NSString *menuName = [_menuArray objectAtIndex:indexPath.row];
-    cell.textLabel.text = menuName;
-    
-    return cell;
-}
-
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return NO;
-}
 
 
 
