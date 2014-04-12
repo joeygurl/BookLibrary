bc494bb489871410e4bfe0f8f69221f7bce0564f
Diorelle Ramos <dramos30@gmail.com>
2014-04-12 12:06:49 -0500

    Adding Main Menu Screen

---
+++ BookLibrary/HomeViewController.h
@@ -0,0 +1,12 @@
+//
+//  HomeViewController.h
+//  BookLibrary
+//
+//  Created by Diorelle Ramos on 4/12/14.
+//  Copyright (c) 2014 Appy Days. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UITableViewController
+@end
---
+++ BookLibrary/HomeViewController.m
@@ -0,0 +1,83 @@
+//
+//  HomeViewController.m
+//  BookLibrary
+//
+//  Created by Diorelle Ramos on 4/12/14.
+//  Copyright (c) 2014 Appy Days. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController (){
+    NSArray *_menuArray;
+}
+
+@end
+
+@implementation HomeViewController
+
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    _menuArray = [NSArray arrayWithObjects:@"My Library",@"Search Library", @"Settings", nil];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table View
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [_menuArray count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    NSString *menuName = [_menuArray objectAtIndex:indexPath.row];
+    cell.textLabel.text = menuName;
+    
+    return cell;
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return NO;
+}
+
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
