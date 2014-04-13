b802d740d83780ce160230a14c51ad67165477f1
aditiy2 <aditiy2@gmail.com>
2014-04-13 14:13:18 -0500

    My Library, My Books, Book Detail and Add Books details screen. I was not able to hook My Library to My books - may be doing something wrong with segue ?

---
+++ BookLibrary/MyLibraryViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyLibraryViewController.h
+//  BookLibrary
+//
+//  Created by Aditi Yagnik on 4/13/14.
+//  Copyright (c) 2014 Appy Days. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyLibraryViewController : UIViewController
+
+@end
---
+++ BookLibrary/MyLibraryViewController.m
@@ -0,0 +1,38 @@
+//
+//  MyLibraryViewController.m
+//  BookLibrary
+//
+//  Created by Aditi Yagnik on 4/13/14.
+//  Copyright (c) 2014 Appy Days. All rights reserved.
+//
+
+#import "MyLibraryViewController.h"
+
+@interface MyLibraryViewController ()
+
+@end
+
+@implementation MyLibraryViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
