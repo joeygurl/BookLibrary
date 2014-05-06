05f8be144c7a12e73fcbb005ad836562e55ad540
Bret Hassler <bret.hassler@gmail.com>
2014-05-05 21:38:32 -0500

    Add Account Info Controllers
    Still need to handle save

---
+++ BookLibrary/AccountDetails.h
@@ -0,0 +1,31 @@
+//
+//  AccountDetails.h
+//  BookLibrary
+//
+//  Created by Bret Hassler on 5/5/14.
+//  Copyright (c) 2014 Appy Days. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Account.h"
+
+@protocol AccountDetailsDelegate;
+@interface AccountDetails : UITableViewController
+
+@property (nonatomic, weak) id delegate;
+//@property (nonatomic, strong) User *userDetail;
+
+- (IBAction)backgroundTap:(id)sender;
+
+
+@end
+
+
+@protocol AccountDetailsDelegate
+
+// delegate methods
+- (void)accountDetailsCancel;
+- (void)accountDetailsSave:(User *)user;
+
+@end
+
